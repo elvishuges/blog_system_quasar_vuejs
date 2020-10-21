e441209bee343cabb9f5029a5d552fc37f05f514
elvishuges <elvishuges@hotmail.com>
2020-10-21 19:46:54 -0300

    adicionando itens de busca na barra de ferramentas

--- src/layouts/FeedLayout.vue
+++ src/layouts/FeedLayout.vue
@@ -1,11 +1,19 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <q-toolbar>
-        <q-toolbar-title>
-          feed
-        </q-toolbar-title>
-        <div></div>
+      <q-toolbar  class="bg-white text-grey">
+        <q-btn class="q-mr-sm" style="color: #767676;" flat label="Página inicial" />
+        <q-btn class="q-mr-sm" style="color: #767676;" flat label="Hoje" />
+        <q-btn class="q-mr-sm" style="color: #767676;" flat label="Seguindo" />
+        <q-input color="greey-12" outlined  borderless dense rounded standout v-model="text" input-class="text-right" class="q-ml-md">
+          <template v-slot:append>
+            <q-icon v-if="text === ''" name="search" />
+            <q-icon v-else name="clear" class="cursor-pointer" @click="text = ''" />
+          </template>
+        </q-input>
+        <q-space></q-space>
+        <q-btn dark flat round dense icon="chat" class="q-mr-xs" />
+        <q-btn flat round dense icon="logout" />
       </q-toolbar>
     </q-header>
     <q-page-container>
@@ -66,6 +74,7 @@ export default {
   name: 'MainLayout',
   data () {
     return {
+       text: '',
       leftDrawerOpen: false,
       essentialLinks: linksData
     }
--- src/store/index.js
+++ src/store/index.js
@@ -33,4 +33,5 @@ export default function (/* { ssrContext } */) {
   }
 
   return Store
+
 }
